6f05922a45d0c45fe5bb89a8c1c823be537721af
Dmitry Chabanenko <chdn6026@mail.ru>
2016-02-17 08:59:35 +0200

    Added main with static functions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+package com.company;
+
 public class Main {
+
     public static void main(String[] args) {
-        LinSolv l = new LinSolv();
-        l.setA(1);
-        l.setB(2);
-        double x = l.solve();
-        System.out.println("x=" + x);
+	// write your code here
+        System.out.println("Program beginning:");
+//        System.out.println("Static field RandomNumbersContainer.N = " + RandomNumbersContainer.N);
+//        RandomNumbersContainer.init();
+//        double x = RandomNumbersContainer.getRandomByPosition(0);
+//        System.out.println("x=" + x);
+//        RandomNumbersContainer.writeRandomsToAFile("randoms.txt");
+        RandomNumbersContainer c1 = new RandomNumbersContainer();
+        RandomNumbersContainer c2 = new RandomNumbersContainer();
+        RandomNumbersContainer c3 = new RandomNumbersContainer();
+        System.out.println("NumberOfObjects = " + c1.getNumberOfObjects());
+
     }
 }
